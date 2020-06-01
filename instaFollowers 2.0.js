36ae2afbae4671c4ffc4d6d15a15718ec936e7c1
NizamKolathoden <59057736+nizamkolathoden@users.noreply.github.com>
2020-06-01 22:36:26 +0530

    Update instaFollowers 2.0.js

--- instaFollowers 2.0.js
+++ instaFollowers 2.0.js
@@ -1,7 +1,7 @@
-function time(){
+
 document.getElementsByClassName("_5f5mN")[0].click();
 document.getElementsByClassName("_5f5mN")[1].click();
 document.getElementsByClassName("HUW1v hUQXy")[0].click();
 for(var i=0;i<=20;i++){
     document.getElementsByClassName("sqdOP  L3NKy   y3zKF     ")[i].click()
-}setInterval(time,5000)}
+}
